35cd04068a89c734b16a68949cd0fa4b642a3f05
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-03-06 21:11:50 -0300

    Create day-1-demo.py

---
+++ day001/day-1-demo.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao Gerador de Nomes de Bandas")
+city = input("Qual é o nome da cidade onde você cresceu?\n")
+name_pet = input("Qual era o nome do seu pet?\n")
+print("O nome da sua banda poderia ser " + city + " " + name_pet)
