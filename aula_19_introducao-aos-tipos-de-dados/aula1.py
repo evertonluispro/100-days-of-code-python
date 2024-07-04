0641f5d292f9759b83e10b38a3b19d9dac2e9706
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:08:01 -0300

    Create aula1.py

---
+++ aula_19_introducao-aos-tipos-de-dados/aula1.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Everton Luis')
+print('Everton "Luis"')
+
+# Aspas duplas
+print("Everton Luis")
+print("Everton 'Luis'")
+
+# Escape
+print("Everton \"Luis\"")
+
+# r
+print(r"Everton \"Luis\"")
