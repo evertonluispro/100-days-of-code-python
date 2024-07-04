de3922373b8bfc6122d6248c7ef0f82f903295dd
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:13:29 -0300

    Create aula35.py

---
+++ aula_35_condicional-if- elife-else-parte2/aula35.py
@@ -0,0 +1,9 @@
+# if /  elif     / else
+# se / se nao se / se não
+condicao = True
+
+if condicao:
+    print('Este é o código do if')
+else:
+    print('Este é o else do primeiro if')
+print('Fora do if')
