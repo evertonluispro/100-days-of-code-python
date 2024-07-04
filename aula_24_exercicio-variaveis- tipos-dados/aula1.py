85f0b911abd26e9d4ab4f3f0ed11d10d365d4eaf
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:11:41 -0300

    Create aula1.py

---
+++ aula_24_exercicio-variaveis- tipos-dados/aula1.py
@@ -0,0 +1,14 @@
+nome = 'Everton'
+sobrenome = 'Luis'
+idade = 25
+ano_nascimento = 1999
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_metros)
+
