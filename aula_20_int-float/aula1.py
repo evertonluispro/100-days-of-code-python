59236decc7b9280484f6477e3b1f57c0ff8a692e
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:08:29 -0300

    Create aula1.py

---
+++ aula_20_int-float/aula1.py
@@ -0,0 +1,20 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo. Int sem sinal é considerado
+# Positivo
+print(11) # int
+print(-11) # int
+print(0)
+
+#  float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+print(1.1)
+
+# A função type mostra o tipo que o Python
+# Interferiu ao valor.
+print(type('Everton'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
