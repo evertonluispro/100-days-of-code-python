67c41729840bc4f3575cb94a203313016ca666de
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:12:22 -0300

    Create aula1.py

---
+++ aula_31_introducao-f-strings/aula1.py
@@ -0,0 +1,8 @@
+nome = 'Everton Luis'
+altura = 1.70
+peso = 68
+imc = peso / altura ** 2
+
+print(f'{nome} tem {altura:.2f} de altura,')
+print(f'pesa {peso} quilos e seu IMC é')
+print(f'{imc:.2f}')
