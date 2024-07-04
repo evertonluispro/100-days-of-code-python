89eacaa739f1e22b9a22cf7f706522aa546aee1c
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:07:48 -0300

    Create aula1.py

---
+++ aula_17_docstrings-como-comentarios/aula1.py
@@ -0,0 +1,12 @@
+"""
+DocString não é um comentário, e posso escrever o que eu quiser
+"""
+
+'''
+Pode ser feito com aspas duplas ou simples, onde você pode escrever explicações para o seu código
+'''
+
+# Permite escrever um comentário
+print(123) # Na Frente
+# Abaixo
+print(456)
