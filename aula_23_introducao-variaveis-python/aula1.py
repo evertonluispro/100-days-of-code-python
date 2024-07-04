6173a896c49610bb1d37abd9612bdab5003c8039
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-03 23:11:35 -0300

    Create aula1.py

---
+++ aula_23_introducao-variaveis-python/aula1.py
@@ -0,0 +1,12 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+nome_completo = 'Everton Luis Santos Silva'
+idade = 25
+maior_de_idade = idade >= 18
+print('Nome:', nome_completo, 'Idade:', idade)
+print('É maior?', maior_de_idade)
+
