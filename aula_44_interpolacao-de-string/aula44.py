e2afa15576ff0da4b86f4501a4854bb375eeff58
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-14 11:47:51 -0300

    Create aula44.py

---
+++ aula_44_interpolacao-de-string/aula44.py
@@ -0,0 +1,12 @@
+"""
+Interpolação báscia de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Everton'
+preco = 1000.95897643
+variavel = '%s, o preço total foi R$%f' % (nome, preco)
+print(variavel)
